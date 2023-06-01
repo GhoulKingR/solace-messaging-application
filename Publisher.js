87a88e09de484af848b1e8e3c9aabab3db08c764
Chigozie Oduah <chigozie@Chigozies-MacBook-Air.local>
2023-06-01 15:23:25 +0100

    Fixed publisher

--- Publisher.js
+++ Publisher.js
@@ -1,73 +1,57 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Messaging application</title>
-    <script src="./solclientjs-10.13.0/lib/solclient-debug.js"></script>
-    <script src="./Publisher.js"></script>
-    <script src="./Subscriber.js"></script>
-    <script>
-        let publisher = null;
-        let subscriber = null;
+"use strict";
+class Publisher {
+  session = null;
+  topicName = '';
+  url = '';
+  userName = '';
+  password = '';
+  vpnName = '';
 
-        window.onload = () => {
-            var factoryProps = new solace.SolclientFactoryProperties();
-            factoryProps.profile = solace.SolclientFactoryProfiles.version10;
-            solace.SolclientFactory.init(factoryProps);
+  constructor(topicName, url, userName, password, vpnName) {
+    this.topicName = topicName;
+    this.url = url;
+    this.userName = userName;
+    this.password = password;
+    this.vpnName = vpnName;
+  }
 
-            const topicName = 'messages';
-            const hosturl = 'http://localhost:8008';
-            const username = 'admin';
-            const pass = 'admin';
-            const vpn = 'default';
-            const user = localStorage.getItem('username');
-
-            if (user === null) {
-                document.location.assign('/');
-                return ;
-            }
-
-            publisher = new Publisher(topicName, hosturl, username, pass, vpn);
-            subscriber = new Subscriber(topicName, hosturl, username, pass, vpn);
-            const chat = document.getElementById('chat');
-
-            publisher.connect();
-            subscriber.connect();
-
-            subscriber.session.on(solace.SessionEventCode.MESSAGE, function (message) {
-                chat.innerHTML += display(message.getBinaryAttachment());
-            });
-
-            document.getElementById('form').onsubmit = (e) => {
-                e.preventDefault();
-                if (publisher !== null && subscriber !== null) {
-                    const element = document.getElementById('message');
-                    const obj = JSON.stringify({message: element.value, user});
-                    publisher.publish(obj);
-                    element.value = '';
-                }
-            }
-        };
-
-        function display(text) {
-            const {user, message} = JSON.parse(text);
-            const isUser = user === localStorage.getItem('username');
-            return `
-                <fieldset>
-                    <legend align="${isUser ? 'right' : 'left'}">${isUser ? 'You' : user }</legend>
-                    <div>${message}</div>
-                </fieldset>
-            `;
+  connect () {
+    if (this.session === null) { // Make sure a session isn't running
+      
+      // Create a session
+      try {
+        const { url, vpnName, userName, password } = this;
+        this.session = solace.SolclientFactory.createSession({ url, vpnName, userName, password });
+      } catch (error) {
+        console.error( error.toString() );
+      }
+      
+      // Define an event listener to handle session disconnection
+      this.session.on(solace.SessionEventCode.DISCONNECTED, () => {
+        if (this.session !== null) {
+          this.session.dispose();
+          this.session = null;
         }
-    </script>
-</head>
-<body>
-    <div id="chat"></div>
-    <form id="form">
-        <input type='text' id='message' placeholder='Enter text...'/>
-        <input type='submit' value='send'/>
-    </form>
-</body>
-</html>
+      });
+      
+      this.session.connect();
+    }
+  }
+
+  publish (messageText) {
+    if (this.session !== null) {
+      // Prepare a message object
+      let message = solace.SolclientFactory.createMessage();
+      message.setDestination( solace.SolclientFactory.createTopicDestination(this.topicName) );
+      message.setBinaryAttachment( messageText );
+      message.setDeliveryMode( solace.MessageDeliveryModeType.DIRECT );
+
+      // Send the message object
+      try {
+        this.session.send(message);
+      } catch (error) {
+        console.log(error.toString());
+      }
+    }
+  }
+}
